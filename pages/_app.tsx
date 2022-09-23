7b234945cbde59be4c10d8978220bb7cc66fd138
tohsaka888 <156132264@qq.com>
2022-09-23 17:30:59 +0800

    ✨ feat(config): SWRConfig

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
+/*
+ * @Author: tohsaka888
+ * @Date: 2022-09-16 13:40:07
+ * @LastEditors: tohsaka888
+ * @LastEditTime: 2022-09-23 11:49:47
+ * @Description: 请填写简介
+ */
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { SWRConfig } from 'swr'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <SWRConfig>
+      <Component {...pageProps} />
+    </SWRConfig>
+  )
 }
 
 export default MyApp
