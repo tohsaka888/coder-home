376e06f70106d1edb50d0d22e0bfebecb75a0ffc
tohsaka888 <156132264@qq.com>
2022-09-23 19:35:47 +0800

    ✨ feat: add github repo link

--- components/common/Navbar/index.tsx
+++ components/common/Navbar/index.tsx
@@ -6,93 +6,99 @@
  * @Description: Navbar
  */
 
-import React, { CSSProperties, useMemo, useState } from 'react'
-import { Button, Layout, Menu } from 'antd'
-import { useRouter } from 'next/router';
-import { BsTrophyFill, BsTwitch } from 'react-icons/bs'
-import { ItemType } from 'antd/lib/menu/hooks/useItems';
-import useGetCompetitionList from 'hooks/services/useGetCompetitionList';
-import { Flex } from 'styles/index.style';
-import Logo from './Logo';
-import { LoginModalShowContext } from './context';
-import dynamic from 'next/dynamic';
-const LoginModal = dynamic(() => import('./LoginModal'), { ssr: false })
+import React, { CSSProperties, useMemo, useState } from "react";
+import { Button, Layout, Menu } from "antd";
+import { useRouter } from "next/router";
+import { BsGithub, BsTrophyFill, BsTwitch } from "react-icons/bs";
+import { ItemType } from "antd/lib/menu/hooks/useItems";
+import useGetCompetitionList from "hooks/services/useGetCompetitionList";
+import { Flex } from "styles/index.style";
+import Logo from "./Logo";
+import { LoginModalShowContext } from "./context";
+import dynamic from "next/dynamic";
+const LoginModal = dynamic(() => import("./LoginModal"), { ssr: false });
 
 function Navbar() {
   const { Header } = Layout;
-  const { list, loading } = useGetCompetitionList()
-  const router = useRouter()
-  const [visible, setVisible] = useState<boolean>(false)
+  const { list, loading } = useGetCompetitionList();
+  const router = useRouter();
+  const [visible, setVisible] = useState<boolean>(false);
 
-  const pathname = router.pathname
+  const pathname = router.pathname;
 
   const style: CSSProperties = useMemo(() => {
-    return pathname === '/'
+    return pathname === "/"
       ? {
-        color: '#fff',
-        background: 'transparent',
-        position: 'fixed',
-        top: '0px',
-        width: '100vw',
-        zIndex: 999
-      }
+          color: "#fff",
+          background: "transparent",
+          position: "fixed",
+          top: "0px",
+          width: "100vw",
+          zIndex: 999,
+        }
       : {
-        background: undefined
-      }
-  }, [pathname])
+          background: undefined,
+        };
+  }, [pathname]);
 
   const items: ItemType[] = useMemo(() => {
     return [
       {
         icon: <BsTrophyFill />,
-        label: <span style={{ marginLeft: '18px' }}>比赛</span>,
-        key: 'competition'
+        label: <span style={{ marginLeft: "18px" }}>比赛</span>,
+        key: "competition",
       },
       {
         icon: <BsTwitch size={15} />,
-        label: <span style={{ marginLeft: '18px' }}>活动</span>,
-        key: 'activity'
-      }
-    ]
-  }, [])
+        label: <span style={{ marginLeft: "18px" }}>活动</span>,
+        key: "activity",
+      },
+    ];
+  }, []);
 
   return (
-    <LoginModalShowContext.Provider
-      value={{ visible, setVisible }}
-    >
-      <Header
-        style={style}
-      >
-        <Flex alignItems='center' justifyContent='space-between'>
-          <Flex alignItems='center'>
+    <LoginModalShowContext.Provider value={{ visible, setVisible }}>
+      <Header style={style}>
+        <Flex alignItems="center" justifyContent="space-between">
+          <Flex alignItems="center">
             <Logo />
             <Menu
-              theme='dark'
-              style={{ background: 'transparent' }}
+              theme="dark"
+              style={{ background: "transparent" }}
               items={items}
-              defaultSelectedKeys={[pathname.split('/')[1]]}
-              mode={'horizontal'}
+              defaultSelectedKeys={[pathname.split("/")[1]]}
+              mode={"horizontal"}
               onSelect={(info) => {
                 if (!loading) {
-                  if (info.key === 'competition') {
-                    router.push(`/competition/${list[0].id}`)
+                  if (info.key === "competition") {
+                    router.push(`/competition/${list[0].id}`);
                   }
                 }
               }}
             />
           </Flex>
-          <Flex>
-            <Button type="primary" shape={'round'} style={{ width: '80px' }}
+          <Flex alignItems="center">
+            <BsGithub
+              size={25}
+              style={{ marginRight: "16px", cursor: "pointer" }}
+              onClick={() => router.push('https://github.com/tohsaka888/coder-home')}
+            />
+            <Button
+              type="primary"
+              shape={"round"}
+              style={{ width: "80px" }}
               onClick={() => {
-                setVisible(true)
+                setVisible(true);
               }}
-            >登录</Button>
+            >
+              登录
+            </Button>
           </Flex>
         </Flex>
       </Header>
-      <LoginModal width={600} height={350} title={'Login'} />
+      <LoginModal width={600} height={350} title={"Login"} />
     </LoginModalShowContext.Provider>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
