9c3400caa32fe3c61889be7c3d21e66130c2ac0f
tohsaka888 <156132264@qq.com>
2022-09-23 19:36:15 +0800

    🦄 refactor: 显示3d模型

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,58 +5,60 @@
  * @LastEditTime: 2022-09-23 14:00:40
  * @Description: HomePage
  */
-import { Loader } from '@react-three/drei'
-import CustomHead from 'components/common/CustomHead'
-import Navbar from 'components/common/Navbar'
-import Slogan from 'components/HomePage/Slogan'
-import LiveBackground from 'components/LiveBackground'
-import { BackgroundContainer } from 'components/LiveBackground/index.style'
-import { competitionUrl } from 'config/baseUrl'
-import type { GetServerSideProps, NextPage } from 'next'
-import { SWRConfig } from 'swr'
+import { Loader } from "@react-three/drei";
+import CustomHead from "components/common/CustomHead";
+import Navbar from "components/common/Navbar";
+import Slogan from "components/HomePage/Slogan";
+import LiveBackground from "components/LiveBackground";
+import { BackgroundContainer } from "components/LiveBackground/index.style";
+import { competitionUrl } from "config/baseUrl";
+import type { GetServerSideProps, NextPage } from "next";
+import { SWRConfig } from "swr";
 
-const Home: NextPage<{ fallback: Record<string, Response> }> = ({ fallback }) => {
+const Home: NextPage<{ fallback: Record<string, Response> }> = ({
+  fallback,
+}) => {
   return (
     <SWRConfig value={{ fallback }}>
       <CustomHead />
       <Navbar />
       <div className="bg" />
       <Slogan
-        title={'常州工学院 程序员之家'}
-        locale={'zh'}
-        style={{ color: '#fff', fontSize: '50px' }}
-        highlightStyle={{ color: '#1890ff', fontSize: '50px' }}
+        title={"常州工学院 程序员之家"}
+        locale={"zh"}
+        style={{ color: "#fff", fontSize: "50px" }}
+        highlightStyle={{ color: "#1890ff", fontSize: "50px" }}
         highlightLength={5}
         cursorStyle={{
-          width: '38px',
-          height: '8px'
+          width: "38px",
+          height: "8px",
         }}
       />
       <Slogan
-        title={'The Home of Coder'}
-        locale={'en'}
-        style={{ color: '#fff', fontSize: '16px' }}
-        cursorStyle={{ width: '16px', height: '5px' }}
+        title={"The Home of Coder"}
+        locale={"en"}
+        style={{ color: "#fff", fontSize: "16px" }}
+        cursorStyle={{ width: "16px", height: "5px" }}
       />
       <BackgroundContainer>
-        {/* <LiveBackground /> */}
+        <LiveBackground />
       </BackgroundContainer>
       <div className="layer" />
       <Loader />
     </SWRConfig>
-  )
-}
+  );
+};
 
 export const getServerSideProps: GetServerSideProps = async () => {
-  const res = await fetch(`${competitionUrl}/api/brief`)
-  const data = await res.json()
+  const res = await fetch(`${competitionUrl}/api/brief`);
+  const data = await res.json();
   return {
     props: {
       fallback: {
-        [`${competitionUrl}/api/brief`]: data
-      }
-    }
-  }
-}
+        [`${competitionUrl}/api/brief`]: data,
+      },
+    },
+  };
+};
 
-export default Home
+export default Home;
