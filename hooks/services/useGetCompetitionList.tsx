4a1203895b3fdfffef51b8197033d90737876c69
tohsaka888 <156132264@qq.com>
2022-09-23 17:30:44 +0800

    ✨ feat(hooks): 获取比赛列表

---
+++ hooks/services/useGetCompetitionList.tsx
@@ -0,0 +1,58 @@
+/*
+ * @Author: tohsaka888
+ * @Date: 2022-09-23 13:19:11
+ * @LastEditors: tohsaka888
+ * @LastEditTime: 2022-09-23 15:24:19
+ * @Description: 获取比赛列表
+ */
+
+import { message } from 'antd';
+import { competitionUrl } from 'config/baseUrl'
+import { useRouter } from 'next/router';
+import React, { useCallback, useMemo } from 'react'
+import useSWR from 'swr';
+
+type Response =
+  | { success: true; list: { id: string; name: string }[] }
+  | { success: false; error: string }
+
+function useGetCompetitionList() {
+
+  const getCompetitionList = useCallback(async (url: string) => {
+    const res = await fetch(url, {
+      method: 'GET',
+      mode: 'cors'
+    })
+    const data: Response = await res.json()
+    return data
+  }, [])
+
+  const { data, error } = useSWR<Response>(`${competitionUrl}/api/brief`, getCompetitionList)
+
+  const router = useRouter()
+
+  const list = useMemo(() => {
+    if (error) {
+      message.error(error)
+      return []
+    }
+    if (data) {
+      if (data?.success) {
+        return data.list
+      } else {
+        message.error(data?.error)
+        return []
+      }
+    } else {
+      return []
+    }
+  }, [data, error])
+
+  if (error) {
+    router.push('/error')
+  }
+
+  return { list, error, loading: !data && !error }
+}
+
+export default useGetCompetitionList
