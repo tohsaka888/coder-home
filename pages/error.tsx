ed9c30d90a30aa5738a05b02e72d8faa7afc1d68
tohsaka888 <156132264@qq.com>
2022-09-23 17:31:11 +0800

    ✨ feat: error page

---
+++ pages/error.tsx
@@ -0,0 +1,17 @@
+/*
+ * @Author: tohsaka888
+ * @Date: 2022-09-23 14:04:59
+ * @LastEditors: tohsaka888
+ * @LastEditTime: 2022-09-23 14:12:30
+ * @Description: ErrorPage
+ */
+
+import React from 'react'
+
+function ErrorPage() {
+  return (
+    <div>Error</div>
+  )
+}
+
+export default ErrorPage
