7b62dfde1639754cb3b279a6ed3644bb5f7cd3ee
tohsaka888 <156132264@qq.com>
2022-09-23 17:31:43 +0800

    ✨ feat(pages): competition page

---
+++ pages/competition/[id].tsx
@@ -0,0 +1,50 @@
+/*
+ * @Author: tohsaka888
+ * @Date: 2022-09-23 11:43:11
+ * @LastEditors: tohsaka888
+ * @LastEditTime: 2022-09-23 15:35:08
+ * @Description: 比赛详情页
+ */
+
+import { Layout, Menu } from 'antd'
+import CustomHead from 'components/common/CustomHead'
+import Loading from 'components/common/Loading'
+import Navbar from 'components/common/Navbar'
+import Detail from 'components/Competition/Detail'
+import useGetCompetitionList from 'hooks/services/useGetCompetitionList'
+import { useRouter } from 'next/router'
+import React from 'react'
+import { BsFillMapFill } from 'react-icons/bs'
+
+function Competition() {
+  const { list, loading } = useGetCompetitionList()
+  const router = useRouter()
+
+  return (
+    <>
+      <CustomHead />
+      <Navbar />
+      <Layout style={{ height: 'calc(100vh - 65px)' }}>
+        <Layout.Sider theme='light' style={{ height: '100%' }}>
+          {loading
+            ? <Loading />
+            : (
+              <Menu
+                defaultSelectedKeys={[(router.query.id as string)]}
+                items={list.map(item => ({ label: item.name, key: item.id, icon: <BsFillMapFill color={'#1890ff'} /> }))}
+                onSelect={(info) => {
+                  router.push('/competition/' + info.key)
+                }}
+              />
+            )
+          }
+        </Layout.Sider>
+        <Layout.Content>
+          <Detail />
+        </Layout.Content>
+      </Layout>
+    </>
+  )
+}
+
+export default Competition
