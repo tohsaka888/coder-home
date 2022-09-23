fec126dfafea545325876cdf8a3aff69f5c03234
tohsaka888 <156132264@qq.com>
2022-09-23 17:30:02 +0800

    ✨ feat(config): baseUrl

--- config/baseUrl.ts
+++ config/baseUrl.ts
@@ -2,8 +2,9 @@
  * @Author: tohsaka888
  * @Date: 2022-09-16 14:17:50
  * @LastEditors: tohsaka888
- * @LastEditTime: 2022-09-16 14:17:51
+ * @LastEditTime: 2022-09-22 08:50:57
  * @Description: baseUrl
  */
 
 export const baseUrl = process.env.NODE_ENV === 'production' ? 'https://' : 'http://localhost:3000'
+export const competitionUrl = 'https://cxy-home-competition.netlify.app'
