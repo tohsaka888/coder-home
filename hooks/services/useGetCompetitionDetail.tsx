d3059f0bec7a4fb6ae6b989b8c18c71fe70b524c
tohsaka888 <156132264@qq.com>
2022-09-25 13:29:25 +0800

    🐞 fix: 修复id为空仍然发送请求

--- hooks/services/useGetCompetitionDetail.tsx
+++ hooks/services/useGetCompetitionDetail.tsx
@@ -6,48 +6,50 @@
  * @Description: 请填写简介
  */
 
-import { message } from 'antd'
-import { competitionUrl } from 'config/baseUrl'
-import { useRouter } from 'next/router'
-import React, { useCallback, useMemo } from 'react'
-import useSWR from 'swr'
+import { message } from "antd";
+import { competitionUrl } from "config/baseUrl";
+import { useRouter } from "next/router";
+import React, { useCallback, useMemo } from "react";
+import useSWR from "swr";
 
 type Response =
-  | { success: true, competition: Competition.Competition }
-  | { success: false, error: string }
+  | { success: true; competition: Competition.Competition }
+  | { success: false; error: string };
 
 function useGetCompetitionDetail(id: string) {
-
-  const router = useRouter()
+  const router = useRouter();
 
   const getCompetitionDetail = useCallback(async (url: string) => {
     const res = await fetch(url, {
-      mode: 'cors'
-    })
-    const data = await res.json()
-    return data
-  }, [])
+      mode: "cors",
+    });
+    const data = await res.json();
+    return data;
+  }, []);
 
-  const { data, error } = useSWR<Response>(`${competitionUrl}/api/competition/${id}`, getCompetitionDetail)
+  const { data, error } = useSWR<Response>(
+    id ? `${competitionUrl}/api/competition/${id}` : null,
+    getCompetitionDetail
+  );
 
   const competition = useMemo(() => {
     if (error) {
-      message.error(error.message)
-      router.push('/error')
-      return
+      message.error(error.message);
+      router.push("/error");
+      return;
     }
     if (data) {
       if (data.success) {
-        return data.competition
+        return data.competition;
       } else {
-        message.error(data.error)
-        router.push('/error')
-        return
+        message.error(data.error);
+        router.push("/error");
+        return;
       }
     }
-  }, [data, error, router])
+  }, [data, error, router]);
 
-  return { competition, error, loading: !data && !error }
+  return { competition, error, loading: !data && !error };
 }
 
-export default useGetCompetitionDetail
+export default useGetCompetitionDetail;
