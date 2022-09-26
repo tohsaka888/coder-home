049510e8fbbc4dcd4e812486ec5b583798369168
tohsaka888 <156132264@qq.com>
2022-09-26 17:12:40 +0800

    🌈 style: 固定比赛头部

--- components/Competition/index.style.ts
+++ components/Competition/index.style.ts
@@ -2,7 +2,7 @@
  * @Author: tohsaka888
  * @Date: 2022-09-23 15:30:34
  * @LastEditors: tohsaka888
- * @LastEditTime: 2022-09-23 15:33:05
+ * @LastEditTime: 2022-09-26 16:53:28
  * @Description: style
  */
 
@@ -12,4 +12,11 @@ export const PartContainer = styled.div`
   background: #fff;
   padding: 8px;
   font-weight: bold;
-`
+  flex: 1;
+`;
+
+export const Container = styled.div`
+  height: calc(100vh - 65px);
+  overflow-x: hidden;
+  overflow-y: auto;
+`;
