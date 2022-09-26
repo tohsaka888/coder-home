8f877a163934ed1581a7db1ef4a2406354d65075
tohsaka888 <156132264@qq.com>
2022-09-26 11:40:51 +0800

    ✨ feat: 提供全局token

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,19 +2,30 @@
  * @Author: tohsaka888
  * @Date: 2022-09-16 13:40:07
  * @LastEditors: tohsaka888
- * @LastEditTime: 2022-09-23 11:49:47
+ * @LastEditTime: 2022-09-26 10:44:46
  * @Description: 请填写简介
  */
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { SWRConfig } from 'swr'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { SWRConfig } from "swr";
+import { TokenContext } from "context";
+import { useEffect, useState } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [token, setToken] = useState<string | null>(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    setToken(token);
+  }, []);
+
   return (
     <SWRConfig>
-      <Component {...pageProps} />
+      <TokenContext.Provider value={{ token, setToken }}>
+        <Component {...pageProps} />
+      </TokenContext.Provider>
     </SWRConfig>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
