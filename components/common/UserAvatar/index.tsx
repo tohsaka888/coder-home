c4c9e98d18d257b8554b100e018f987ef3a8d2cd
tohsaka888 <156132264@qq.com>
2022-09-26 11:37:40 +0800

    ✨ feat: 用户头像组件

---
+++ components/common/UserAvatar/index.tsx
@@ -0,0 +1,47 @@
+/*
+ * @Author: tohsaka888
+ * @Date: 2022-09-26 10:04:52
+ * @LastEditors: tohsaka888
+ * @LastEditTime: 2022-09-26 11:37:12
+ * @Description: UserAvatar
+ */
+
+import React from "react";
+import { motion } from "framer-motion";
+// import { Popover } from "antd";
+import { AvatarContainer } from "./index.style";
+
+type Props = {
+  username: string;
+};
+
+function UserAvatar({ username }: Props) {
+  return (
+    <AvatarContainer>
+      {/* <Popover placement="bottom"> */}
+        <motion.div
+          initial={{
+            width: "30px",
+            height: "30px",
+            borderRadius: "50%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: "#1890ff",
+            color: "#ffffff",
+            fontWeight: "bolder",
+            cursor: "pointer",
+          }}
+          whileHover={{
+            width: "38px",
+            height: "38px",
+          }}
+        >
+          {username[0].toUpperCase()}
+        </motion.div>
+      {/* </Popover> */}
+    </AvatarContainer>
+  );
+}
+
+export default UserAvatar;
