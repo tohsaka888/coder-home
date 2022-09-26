b1beb4812b1a08ecbd04441b4d7e4183d9b5bc45
tohsaka888 <156132264@qq.com>
2022-09-26 12:59:05 +0800

    🦄 refactor(context): 全局存放context

--- context/index.tsx
+++ context/index.tsx
@@ -2,15 +2,12 @@
  * @Author: tohsaka888
  * @Date: 2022-09-26 10:42:01
  * @LastEditors: tohsaka888
- * @LastEditTime: 2022-09-26 10:43:03
+ * @LastEditTime: 2022-09-26 12:53:33
  * @Description: context
  */
 
 import React, { createContext } from "react";
 
-type Props = {
-  token: string | null;
-  setToken: React.Dispatch<React.SetStateAction<string | null>>;
-};
-
-export const TokenContext = createContext<Props | null>(null);
+export const LoginModalShowContext =
+  createContext<Context.LoginModalProps | null>(null);
+export const TokenContext = createContext<Context.TokenProps | null>(null);
