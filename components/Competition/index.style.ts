550ee1a873eae46df079a22d4c611234a0cab95c
tohsaka888 <156132264@qq.com>
2022-09-29 10:58:25 +0800

    🌈 style: 增加PartTitle样式

--- components/Competition/index.style.ts
+++ components/Competition/index.style.ts
@@ -2,7 +2,7 @@
  * @Author: tohsaka888
  * @Date: 2022-09-23 15:30:34
  * @LastEditors: tohsaka888
- * @LastEditTime: 2022-09-26 16:53:28
+ * @LastEditTime: 2022-09-28 16:50:26
  * @Description: style
  */
 
@@ -12,11 +12,18 @@ export const PartContainer = styled.div`
   background: #fff;
   padding: 8px;
   font-weight: bold;
-  flex: 1;
 `;
 
 export const Container = styled.div`
   height: calc(100vh - 65px);
   overflow-x: hidden;
   overflow-y: auto;
+`;
+
+export const PartTitleContainer = styled.div`
+  padding: 4px;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #dfdfdf;
+  width: 100%;
 `;
