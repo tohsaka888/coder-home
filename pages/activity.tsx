5a74a2ad03eecd257b4f245ebde166841398cf99
tohsaka888 <156132264@qq.com>
2022-09-29 16:17:29 +0800

    ✨ feat(pages): 活动页

---
+++ pages/activity.tsx
@@ -0,0 +1,22 @@
+/*
+ * @Author: tohsaka888
+ * @Date: 2022-09-29 16:14:49
+ * @LastEditors: tohsaka888
+ * @LastEditTime: 2022-09-29 16:14:50
+ * @Description: 活动页
+ */
+
+import CustomHead from "components/common/CustomHead";
+import Navbar from "components/common/Navbar";
+import React from "react";
+
+function ActivityPage() {
+  return (
+    <>
+      <CustomHead />
+      <Navbar />
+    </>
+  );
+}
+
+export default ActivityPage;
