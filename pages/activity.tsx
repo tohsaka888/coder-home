80255e7632d5d26de350996b9d86729fd20c9318
tohsaka888 <156132264@qq.com>
2022-09-29 17:15:18 +0800

    ✨ feat: 配置活动详情页

--- pages/activity.tsx
+++ pages/activity.tsx
@@ -2,10 +2,12 @@
  * @Author: tohsaka888
  * @Date: 2022-09-29 16:14:49
  * @LastEditors: tohsaka888
- * @LastEditTime: 2022-09-29 16:14:50
+ * @LastEditTime: 2022-09-29 17:04:46
  * @Description: 活动页
  */
 
+import { Layout } from "antd";
+import Detail from "components/Activity/Detail";
 import CustomHead from "components/common/CustomHead";
 import Navbar from "components/common/Navbar";
 import React from "react";
@@ -15,6 +17,9 @@ function ActivityPage() {
     <>
       <CustomHead />
       <Navbar />
+      <Layout.Content>
+        <Detail />
+      </Layout.Content>
     </>
   );
 }
