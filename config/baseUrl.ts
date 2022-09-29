5978879597b7c2ad72a8c615c726a885457bbe26
tohsaka888 <156132264@qq.com>
2022-09-29 17:14:57 +0800

    ✨ feat(config): 配置活动地址

--- config/baseUrl.ts
+++ config/baseUrl.ts
@@ -2,14 +2,16 @@
  * @Author: tohsaka888
  * @Date: 2022-09-16 14:17:50
  * @LastEditors: tohsaka888
- * @LastEditTime: 2022-09-26 09:49:23
+ * @LastEditTime: 2022-09-29 16:38:43
  * @Description: baseUrl
  */
 
 export const baseUrl =
   process.env.NODE_ENV === "production"
     ? "https://coder-home.netlify.app"
-    // ? "http://localhost:3000"
-    : "http://localhost:3000";
+    : // ? "http://localhost:3000"
+      "http://localhost:3000";
+
+export const activityUrl = `https://cxy-home-activity.netlify.app`;
 export const competitionUrl = "https://cxy-home-competition.netlify.app";
 export const loginUrl = "https://cxy-home-login.netlify.app";
