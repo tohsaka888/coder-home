44b913f2f93d5cf9cbb74c9ee0a7886cc1642262
tohsaka888 <156132264@qq.com>
2022-09-29 13:27:06 +0800

    ✨ feat: antd locale配置

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@
  * @Author: tohsaka888
  * @Date: 2022-09-16 13:40:07
  * @LastEditors: tohsaka888
- * @LastEditTime: 2022-09-26 10:44:46
+ * @LastEditTime: 2022-09-29 12:45:20
  * @Description: 请填写简介
  */
 import "../styles/globals.css";
@@ -10,6 +10,8 @@ import type { AppProps } from "next/app";
 import { SWRConfig } from "swr";
 import { TokenContext } from "context";
 import { useEffect, useState } from "react";
+import { ConfigProvider } from "antd";
+import zhCN from "antd/lib/locale/zh_CN";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [token, setToken] = useState<string | null>(null);
@@ -21,9 +23,11 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <SWRConfig>
-      <TokenContext.Provider value={{ token, setToken }}>
-        <Component {...pageProps} />
-      </TokenContext.Provider>
+      <ConfigProvider locale={zhCN}>
+        <TokenContext.Provider value={{ token, setToken }}>
+          <Component {...pageProps} />
+        </TokenContext.Provider>
+      </ConfigProvider>
     </SWRConfig>
   );
 }
