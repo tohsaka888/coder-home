74c6f5844cec2ae83c93be1c79c80efb757ca55f
tohsaka888 <156132264@qq.com>
2023-02-20 16:45:45 +0800

    ♻️ refactor: 重新配置域名

--- config/baseUrl.ts
+++ config/baseUrl.ts
@@ -8,10 +8,10 @@
 
 export const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://coder-home.netlify.app"
+    ? "https://coder-home.tohsaka888.asia"
     : // ? "http://localhost:3000"
       "http://localhost:3000";
 
-export const activityUrl = `https://cxy-home-activity.netlify.app`;
-export const competitionUrl = "https://cxy-home-competition.netlify.app";
-export const loginUrl = "https://cxy-home-login.netlify.app";
+export const activityUrl = `https://cxy-home-activity.tohsaka888.asia`;
+export const competitionUrl = "https://cxy-home-competition.tohsaka888.asia";
+export const loginUrl = "https://cxy-home-login.tohsaka888.asia";
